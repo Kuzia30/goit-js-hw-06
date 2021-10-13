23353ad5c2e0a974d52860fead9c9952b29fa55c
Kuzia30 <maxzimka37@gmail.com>
2021-10-13 19:05:10 +0300

    add task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const widgetEl = document.querySelector('.widget');
+const bodyEl = widgetEl.parentNode;
+const colorTextEl = document.querySelector('.color');
+const changeColorBtnEl = document.querySelector('.change-color');
+
+changeColorBtnEl.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorTextEl.textContent = getRandomHexColor();
+});
+
