9733acf0aa1c64914bce515e6dd161bfa1d32737
Kuzia30 <maxzimka37@gmail.com>
2021-10-14 15:12:36 +0300

    fix task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const sizeControlEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
+textEl.style.fontSize = sizeControlEl.value + 'px';
 sizeControlEl.addEventListener('input', onSizeTextChange);
 function onSizeTextChange(event) {
 textEl.style.fontSize = event.currentTarget.value + 'px'
